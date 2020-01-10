efc65f74dabf93a6fa0ce730fba6dc17e7b8e063
liuyihao <yihao123698745@126.com>
2020-01-10 11:44:12 +0800

    111

--- pages/activity/list/activityList.js
+++ pages/activity/list/activityList.js
@@ -1,4 +1,4 @@
-// pages/activity/list/activityList.js
+const app = getApp()
 Page({
 
   /**
@@ -14,80 +14,78 @@ Page({
     stateStrArr: ['状态', '报名中', '即将开始', '报名结束'],
     positionStr: "地点",
     positionStrArr: ['地点', '蓬江区', '江海区', '新会区', '开平市', '恩平市', '台山市', '鹤山市'],
-    data: {
-      activityList: [
-        {
-          id: 1,
-          mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
-          title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
-          date: "02/15(周六)",
-          position: "蓬江区"
-        },
-        {
-          id: 2,
-          mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
-          title: "喜茶喜茶喜茶喜茶",
-          date: "02/15(周六)",
-          position: "蓬江区"
-        },
-        {
-          id: 3,
-          mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
-          title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
-          date: "02/15(周六)",
-          position: "蓬江区"
-        },
-        {
-          id: 4,
-          mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
-          title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
-          date: "02/15(周六)",
-          position: "蓬江区"
-        },
-        {
-          id: 3,
-          mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
-          title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
-          date: "02/15(周六)",
-          position: "蓬江区"
-        },
-        {
-          id: 4,
-          mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
-          title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
-          date: "02/15(周六)",
-          position: "蓬江区"
-        },
-        {
-          id: 3,
-          mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
-          title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
-          date: "02/15(周六)",
-          position: "蓬江区"
-        },
-        {
-          id: 4,
-          mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
-          title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
-          date: "02/15(周六)",
-          position: "蓬江区"
-        },
-        {
-          id: 3,
-          mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
-          title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
-          date: "02/15(周六)",
-          position: "蓬江区"
-        },
-        {
-          id: 4,
-          mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
-          title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
-          date: "02/15(周六)",
-          position: "蓬江区"
-        },
-      ],
-    }
+    data: [
+      {
+        id: 1,
+        mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
+        title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
+        date: "02/15(周六)",
+        position: "蓬江区"
+      },
+      {
+        id: 2,
+        mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
+        title: "喜茶喜茶喜茶喜茶",
+        date: "02/15(周六)",
+        position: "蓬江区"
+      },
+      {
+        id: 3,
+        mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
+        title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
+        date: "02/15(周六)",
+        position: "蓬江区"
+      },
+      {
+        id: 4,
+        mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
+        title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
+        date: "02/15(周六)",
+        position: "蓬江区"
+      },
+      {
+        id: 3,
+        mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
+        title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
+        date: "02/15(周六)",
+        position: "蓬江区"
+      },
+      {
+        id: 4,
+        mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
+        title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
+        date: "02/15(周六)",
+        position: "蓬江区"
+      },
+      {
+        id: 3,
+        mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
+        title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
+        date: "02/15(周六)",
+        position: "蓬江区"
+      },
+      {
+        id: 4,
+        mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
+        title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
+        date: "02/15(周六)",
+        position: "蓬江区"
+      },
+      {
+        id: 3,
+        mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
+        title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
+        date: "02/15(周六)",
+        position: "蓬江区"
+      },
+      {
+        id: 4,
+        mainImage: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3151958140,3815610019&fm=26&gp=0.jpg",
+        title: "喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶喜茶",
+        date: "02/15(周六)",
+        position: "蓬江区"
+      },
+    ],
   },
 
   bindChoiceState: function () {
@@ -114,6 +112,7 @@ Page({
       stateOptionIndex: state,
       stateStr: this.data.stateStrArr[state],
     })
+    this.getViewData();
   },
 
   bindTapPosition: function (e) {
@@ -124,6 +123,7 @@ Page({
       positionOptionIndex: position,
       positionStr: this.data.positionStrArr[position],
     })
+    this.getViewData();
   },
 
   bindActivityTap: function (e) {
@@ -133,11 +133,44 @@ Page({
     })
   },
 
+
+  getViewData() {
+    let _this = this;
+
+    wx.showLoading({
+      title: "",
+    })
+
+    wx.request({
+      url: app.globalData.url + '/server/activity/activity/list/' + _this.data.stateOptionIndex + '/' + _this.data.positionOptionIndex,
+      method: "GET",
+      header: {
+        'token': JSON.parse(wx.getStorageSync('session')).token
+      },
+      success(res) {
+        if (res.statusCode == 200 && res.data.code == 200) {
+          let data = res.data.data;
+          _this.setData({
+            data: data
+          })
+        } else {
+          wx.showModal({
+            title: '失败',
+            content: '请稍后再试',
+          })
+        }
+      },
+      complete() {
+        wx.hideLoading()
+      }
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getViewData()
   },
 
   /**
